0c800441e059391ce98ce42f39e7640638a6079b
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-09-21 18:17:25 -0600

    Update Motor.java

--- Practica3/Motor.java
+++ Practica3/Motor.java
@@ -1,75 +1,90 @@
 import java.util.Random;
 
 public class Motor {
-    private int revoluciones;
-    private int aire;
-    private int combustible;
-    private boolean piston;
-    private boolean bujia;
-    private boolean encendido;
-    private Random r;
+	
+	private int revoluciones;		// La cantidad de ciclos por segundo que el motor está realizando.
+	private int aire;				// Volumen en mm3 del aire que entra a un cilindro del motor.
+	private int combustible;		// Volumen en mm3 de gasolina que entra a un cilindro del motor
+	private boolean piston;		// Indica si el pistón del cilindro está arriba (on) o abajo (on)
+	private boolean bujia;			// Indica si la bujía del cilindro está encendida o apagada
+	private boolean encendido;		// Indica si el motor se encuentra funcionando
+	private Random r;			// Utilizado para motivos de simulación del entorno del motor.
+	
+	/**
+	* Inicializa un nuevo objeto Motor.
+	*/
+	public Motor() {
+		aire = 0;
+		combustible = 0;
+		revoluciones = 0;
+		encendido = false;
+		r = new Random();
+	}
+	
+	/**
+	* Llena el cilindro con la cantidad y proporción necesarias de aire y combustible para que se realize la combustión.
+	*/
+	private void prepararMezcla() {
+		while(!(Math.abs(aire - combustible) < 5 && (aire > 50 && combustible > 50))){
+			aire += r.nextInt(10);
+			combustible += r.nextInt(10);
+		}
+		piston = false;
+	}
+	
+	/**
+	* Sube el cilindro para comprimir la mezcla de aire y combustible. Además se asegura que la bujía esté apagada.
+	*/
+	private void comprimirMezcla(){
+		piston = true;
+		bujia = false;
+	}
+	
+	/**
+	* Enciende la bujía el tiempo necesario para que la combustión se realice y la apaga después. 
+	* Posteriormente, convierte la energía química de la combustión en energía mecánica, que se
+	* transifere al cigüeñal para aumentar las revoluciones del motor. El resto de la energía química
+	* se elimina a través del escape.
+	*/
+	private void encenderMezcla(){
+		bujia = true;
+		bujia = false;
+		double energia = (aire + combustible) * 34.78 / 2;
+		revoluciones += (int)(energia / 100);
+		piston = false;
+		eliminarDesechos(energia);
+	}
+	
+	/**
+	* Elimina los desechos producidos tras realizar la combustión en el cilindro.
+	*/
+	private void eliminarDesechos(double residuos){
+		while(residuos > 0){
+			residuos -= r.nextDouble();
+		}
+	}
+	public void revolucionar() {
+		if(encendido) {
+			prepararMezcla();
+			comprimirMezcla();
+			encenderMezcla();
+		}
+		}
+	
+	public boolean getEncendido(){
+		return encendido;
+	}
 
-    public Motor() {
-        aire = 0;
-        combustible = 0;
-        revoluciones = 0;
-        setEncendido(false);
-        r = new Random();
-    }
+	public int getRevoluciones(){
+		return revoluciones;
+	}
+	
+	public void setEncendido(boolean encendido){
+		this.encendido = encendido;
+	}
 
-    public void setEncendido(boolean encendido) {
-        this.encendido = encendido;
-    }
-
-    private void prepararMezcla() {
-        while (!(Math.abs(aire - combustible) < 5 && (aire > 50 && combustible > 50))) {
-            aire += r.nextInt(10);
-            combustible += r.nextInt(10);
-        }
-        piston = false;
-    }
-
-    private void comprimirMezcla() {
-        piston = true;
-        bujia = false;
-    }
-
-    private void encenderMezcla() {
-        bujia = true;
-        bujia = false;
-        double energia = (aire + combustible) * 34.78 / 2;
-        revoluciones += (int) (energia / 100);
-        piston = false;
-        eliminarDesechos(energia);
-    }
-
-    private void eliminarDesechos(double residuos) {
-        while (residuos > 0) {
-            residuos -= r.nextDouble();
-        }
-    }
-
-    // Getter para encendido
-    public boolean isEncendido() {
-        return encendido;
-    }
-
-    // Getter para revoluciones
-    public int getRevoluciones() {
-        return revoluciones;
-    }
-
-    // Nuevo método para manejar las revoluciones
-    public void setRevoluciones(int cantidad) {
-        revoluciones = cantidad;
-    }
-
-    // Nuevo método para realizar las operaciones necesarias
-    public void revolucionar() {
-        if (isEncendido()) {
-            prepararMezcla();
-            comprimirMezcla();
-            encenderMezcla();
-        }
-    }
+	public void setRevoluciones(int cantidad){
+		this.revoluciones = cantidad;
+	}
+	
 }
