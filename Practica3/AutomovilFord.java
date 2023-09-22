3fbacfeed2cd75b94b16a0ac1cbcd27e14a76b9c
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-09-21 18:16:56 -0600

    Update AutomovilFord.java

--- Practica3/AutomovilFord.java
+++ Practica3/AutomovilFord.java
@@ -1,48 +1,87 @@
+/**
+* Automovil.java
+*
+* @author Héctor Quej Cosgaya
+* @author José Aguilar Canepa
+*
+* @created 05/11/12 10:50 PM
+*
+* Esta clase modela exclusivamente automoviles de la marca "Ford".
+*/
 public class AutomovilFord {
-    int velocidad;
-    String color;
-    public String modelo;
-    Motor motor;
-    public static final String marca = "Ford";
-
-    public AutomovilFord(String modelo, String color) {
-        this.modelo = modelo;
-        this.color = color;
-        velocidad = 0;
-        motor = new Motor();
-    }
-
-    public AutomovilFord() {
-        this("EcoSport", "Azul");
-    }
-
-    public void encenderMotor() {
-        motor.setEncendido(true);
-        System.out.println("Motor encendido");
-    }
-
-    public void apagarMotor() {
-        motor.setEncendido(false);
-        System.out.println("Motor apagado");
-    }
-
-    public void acelerar() {
-        if (motor.isEncendido()) {
-            motor.revolucionar(); // Llama al método revolucionar() en Motor
-            velocidad = (motor.getRevoluciones() / 100);
-            System.out.println("Vamos a " + velocidad + " kph");
-        } else {
-            System.out.println("El motor está apagado!");
-        }
-    }
-
-    public void frenar(int cantidad) {
-        motor.setRevoluciones(cantidad); // Llama al setter de revoluciones en Motor
-        if (velocidad < 0) velocidad = 0;
-        System.out.println("Vamos a " + velocidad + " kph");
-    }
-
-    public int getVelocidad() {
-        return velocidad;
-    }
+	
+	int velocidad;						// La velocidad a la que se desplaza el auto
+	String color;						// El color del auto
+	public String modelo;					// El modelo del auto
+	Motor motor;						// El motor del auto
+	public String marca = "Ford";	// La marca del auto
+		
+	/**
+	* Inicializa un nuevo objeto Automovil.
+	* 
+	* @param modelo: El modelo del auto.
+	* @param color: El color del auto
+	*/
+	public AutomovilFord(String modelo, String color){
+		this.modelo = modelo;
+		this.color = color;
+		velocidad = 0;
+		motor = new Motor();
+	}
+	
+	/**
+	* Inicializa un nuevo objeto Automovil.
+	*/
+	public AutomovilFord(){
+		this("EcoSport", "Azul");
+	}
+	
+	/**
+	* Enciende el motor del automovil.
+	*/
+	public void encenderMotor() {
+		motor.setEncendido(true);
+		System.out.println("Motor encendido");
+	}
+	
+	/**
+	* Apaga el motor del automovil.
+	*/
+	public void apagarMotor() {
+		motor.setEncendido(false);
+		System.out.println("Motor apagado");
+	}
+	
+	/**
+	* Incrementa la velocidad del automovil en la cantidad de kilómetros/hora especificada.
+	*
+	* @param cantidad: El número de km/h en que aumentará la velocidad del auto.
+	*/
+	public void acelerar() {
+		if (motor.getEncendido()) {
+			motor.revolucionar();	
+			velocidad = (motor.getRevoluciones() / 100);
+			System.out.println("Vamos a " + velocidad + " kph");			
+		} else {
+			System.out.println("El motor está apagado!");
+		}
+	}
+	
+	/**
+	* Reduce a cero la velocidad del auto.
+	*/
+	public void frenar(int cantidad) {
+		motor.setRevoluciones(cantidad);
+		if(velocidad < 0) velocidad = 0;
+		System.out.println("Vamos a " + velocidad + " kph");	
+	}
+	
+	/**
+	* Nos muestra la velocidad a la que se está desplazando el auto.
+	* 
+	* @return la velocidad del auto.
+	*/
+	public int getVelocidad() {
+		return velocidad;
+	}
 }
