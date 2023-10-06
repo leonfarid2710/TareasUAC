d5f5b2fbf4d05866bee5108c1c6bc90e35fff527
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-10-06 08:44:08 -0600

    Create PokemonAgua.java

---
+++ Pokemon_Herencia/PokemonAgua.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package batallapokemon;
+
+/**
+ *
+ * @author Alumno
+ */
+public class PokemonAgua extends Pokemon {
+    public PokemonAgua(String nombre, int nivel) {
+        super(nombre, "Agua", nivel);
+    }
+}
