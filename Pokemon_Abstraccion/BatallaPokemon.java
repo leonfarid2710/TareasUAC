62395f28b99c8ab2d6a7c855193d7a5c56c6a2a3
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-10-13 13:39:46 -0600

    Update BatallaPokemon.java

--- Pokemon_Abstraccion/BatallaPokemon.java
+++ Pokemon_Abstraccion/BatallaPokemon.java
@@ -1,1 +1,17 @@
+import pokemon.PokemonFuego;
+import pokemon.PokemonAgua;
+public class BatallaPokemon {
+
+     public static void main(String[] args) {
+        
+        PokemonAgua squirtle = new PokemonAgua("Squirtle", 10);
+        PokemonFuego magmar = new PokemonFuego("magmar", 10);
+
+       
+        squirtle.atacar(0, magmar);
+        System.out.println("\n");
+        magmar.atacar(3, squirtle);
+       
+    }
+}
 
