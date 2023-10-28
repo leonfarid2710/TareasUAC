435eee201526842bdc5a70bb6482f94a01522c05
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-10-27 20:00:14 -0600

    Update Squirtle.java

--- Pokemon_Polimorfismo/Pokemon/Squirtle.java
+++ Pokemon_Polimorfismo/Pokemon/Squirtle.java
@@ -9,6 +9,7 @@ public class Squirtle extends Pokemon {
 
         setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Burbuja"));
         setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Hidropulso"));
+        
         setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Tackle"));
         setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Skull Bash"));
     }
@@ -17,19 +18,19 @@ public class Squirtle extends Pokemon {
     public double obtenerEfectividad(Pokemon pokemonAtacante, int m){
         double efectividad = 1.0;
         if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.FUEGO) efectividad = 2.0;
-        if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.VENENO) efectividad = 0.5;
-        if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.PLANTA) efectividad = 0.5;
-        if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.ROCA) efectividad = 2.0;
-        if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.TIERRA) efectividad = 2.0;
+            if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.VENENO) efectividad = 0.5;
+             if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.PLANTA) efectividad = 0.5;
+                if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.ROCA) efectividad = 2.0;
+                     if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.TIERRA) efectividad = 2.0;
 
         return efectividad;
     }
 
     public Tipo getTipo() {
-        return tipo;
+    return tipo;
     }
 
     public void setTipo(Tipo tipo) {
-        this.tipo = tipo;
+    this.tipo = tipo;
     }
 }
