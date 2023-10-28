6de90ee188241d7b91d0a81278d22428c174ad3e
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-10-27 19:57:13 -0600

    Update BatallaPokemon.java

--- Pokemon_Polimorfismo/BatallaPokemon.java
+++ Pokemon_Polimorfismo/BatallaPokemon.java
@@ -5,10 +5,10 @@ import pokemon.Bulbasur;
 public class BatallaPokemon {
     public static void main(String args[]){
         Pokemon bulbasur = new Bulbasur("bulbasur", 1);
-        Pokemon squirtle = new Squirtle("Squirtle", 1);
+            Pokemon squirtle = new Squirtle("Squirtle", 1);
 
         bulbasur.atacar(3, squirtle, bulbasur);
-        System.out.println("\n");
+            System.out.println("\n");
         squirtle.atacar(3, bulbasur, squirtle);
     }
 }
