604de8d1e34bd77c6c87d06feb62b6a0dda8aa8d
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-10-27 19:59:17 -0600

    Update Movimiento.java

--- Pokemon_Polimorfismo/Pokemon/Movimiento.java
+++ Pokemon_Polimorfismo/Pokemon/Movimiento.java
@@ -1,41 +1,42 @@
 package pokemon;
 
 public class Movimiento {
-    private String nombre;
-    private int potencia, pp;
-    private Tipo tipo;
-    private TipodeMovimiento claseDeMovimiento;
+private String nombre;
+private int potencia, pp;
+private Tipo tipo;
+private TipodeMovimiento claseDeMovimiento;
     
-    public Movimiento(String nombre, int potencia, int pp, Tipo tipo, TipodeMovimiento claseDeMovimiento) {
-        this.nombre = nombre;
-        this.potencia = potencia;
-        this.pp = pp;
-        this.tipo = tipo;
-        this.claseDeMovimiento = claseDeMovimiento;
+public Movimiento(String nombre, int potencia, int pp, Tipo tipo, TipodeMovimiento claseDeMovimiento) {
+this.nombre = nombre;
+this.potencia = potencia;
+this.pp = pp;
+this.tipo = tipo;
+this.claseDeMovimiento = claseDeMovimiento;
     }
 
-    public String getNombre() {
-        return nombre;
+public String getNombre() {
+return nombre;
     }
 
-    public int getPotencia() {
-        return potencia;
+public int getPotencia() {
+return potencia;
     }
 
-    public int getPp() {
-        return pp;
+public int getPp() {
+return pp;
     }
 
-    public Tipo getTipo() {
-        return tipo;
+public Tipo getTipo() {
+return tipo;
     }
 
-    public TipodeMovimiento getClaseDeMovimiento() {
-        return claseDeMovimiento;
+public TipodeMovimiento getClaseDeMovimiento() {
+return claseDeMovimiento;
     }
 
-    public void setPp(int pp) {
-        this.pp = pp;
+public void setPp(int pp) {
+this.pp = pp;
     }
 
 }
+
