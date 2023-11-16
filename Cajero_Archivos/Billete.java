f94754c848dea71f75596ceb5b0aca0bc68ad523
leonfarid2710 <145058912+leonfarid2710@users.noreply.github.com>
2023-11-15 19:40:52 -0600

    Update Billete.java

--- Cajero_Archivos/Billete.java
+++ Cajero_Archivos/Billete.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 
 public class Billete implements Serializable {
-    private static final long serialVersionUID = 123456789L; // Cambia este número según tus necesidades
+    private static final long serialVersionUID = 123456789L; // Cambiar este número según los criterios necesarios
     private int denominacion;
     private int cantidad;
 
